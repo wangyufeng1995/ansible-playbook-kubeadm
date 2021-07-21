476a1e518330b375d8f8001f2bd95bfc5b2f476e
运维界的乔治 <38524878+wangyufeng1995@users.noreply.github.com>
2021-07-21 09:48:43 +0800

    Update Ansible_keyless_interaction.sh

--- ansible/Ansible_keyless_interaction.sh
+++ ansible/Ansible_keyless_interaction.sh
@@ -4,24 +4,32 @@
 #Notice: Ansible keyless interaction
 IP=`cat /etc/ansible/hosts | grep -Ev "#|$^" | grep -vE "[a-z]"`
 #输入服务器密码
-password="password"
+password="pd"
 
 yum nstall sshpass ansible -y
 
 ssh-keygen -t dsa   -f ~/.ssh/id_dsa -P ''
 
+successed_list=( )
+failed_list=( )
+n=0
 #fenfa
 for i in ${IP[@]}
 do
-/usr/bin/sshpass -p${password} ssh-copy-id -oStrictHostKeyChecking=no ${i}
-done 
-
-if [ $? -eq 0 ];then
-	echo "=============================免密成功=================================="
-else
-	echo "免密失败"
-	exit 1 
-fi
-
+	/usr/bin/sshpass -p${password} ssh-copy-id -oStrictHostKeyChecking=no ${i} > /dev/null
+	let n++
+	if [ $? -eq 0 ];then
+		successed_list[$n]=$i
+	else
+		failed_list[$n]=$i
+	fi
+done
 
 /usr/bin/ansible "*" -m ping
+
+for success in ${successed_list[@]};
+do
+	echo 免密成功主机: ${success};
+done
+
+echo 免密失败主机个数: ${#failed_list[*]}
